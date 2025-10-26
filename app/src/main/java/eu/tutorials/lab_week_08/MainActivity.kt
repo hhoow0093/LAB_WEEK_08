47291a96c2b44ae1b757f09e02963a566ee77ed6
hhoow0093 <howarddai2005@gmail.com>
2025-10-27 00:53:46 +0700

    Part 2 - Tracking your Background Work with a Foreground Service

--- app/src/main/java/eu/tutorials/lab_week_08/MainActivity.kt
+++ app/src/main/java/eu/tutorials/lab_week_08/MainActivity.kt
@@ -1,9 +1,11 @@
 package eu.tutorials.lab_week_08
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.work.Constraints
@@ -15,54 +17,28 @@ import eu.tutorials.lab_week_08.worker.FirstWorker
 import eu.tutorials.lab_week_08.worker.SecondWorker
 
 class MainActivity : AppCompatActivity() {
-    //Create an instance of a work manager
-//Work manager manages all your requests and workers
-//it also sets up the sequence for all your processes
     private val workManager = WorkManager.getInstance(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//Create a constraint of which your workers are bound to.
-//Here the workers cannot execute the given process if
-//there's no internet connection
         val networkConstraints = Constraints.Builder()
             .setRequiredNetworkType(NetworkType.CONNECTED)
             .build()
         val Id = "001"
-//There are two types of work request:
-//OneTimeWorkRequest and PeriodicWorkRequest
-//OneTimeWorkRequest executes the request just once
-//PeriodicWorkRequest executed the request periodically
-//Create a one time work request that includes
-//all the constraints and inputs needed for the worker
-//This request is created for the FirstWorker class
         val firstRequest = OneTimeWorkRequest
             .Builder(FirstWorker::class.java)
             .setConstraints(networkConstraints)
             .setInputData(getIdInputData(FirstWorker
                 .INPUT_DATA_ID, Id)
             ).build()
-//This request is created for the SecondWorker class
         val secondRequest = OneTimeWorkRequest
             .Builder(SecondWorker::class.java)
             .setConstraints(networkConstraints)
             .setInputData(getIdInputData(SecondWorker
                 .INPUT_DATA_ID, Id)
             ).build()
-//Sets up the process sequence from the work manager instance
-//Here it starts with FirstWorker, then SecondWorker
         workManager.beginWith(firstRequest).then(secondRequest)
             .enqueue()
-//All that's left to do is getting the output
-//Here, we receive the output and displaying the result as a toast message
-//You may notice the keyword "LiveData" and "observe"
-//LiveData is a data holder class in Android Jetpack
-//that's used to make a more reactive application
-//the reactive of it comes from the observe keyword,
-//which observes any data changes and immediately update the app UI
-//Here we're observing the returned LiveData and getting the
-//state result of the worker (Can be SUCCEEDED, FAILED, or CANCELLED)
-//isFinished is used to check if the state is either SUCCEEDED or FAILE
         workManager.getWorkInfoByIdLiveData(firstRequest.id)
             .observe(this) { info ->
                 info?.let {
@@ -76,17 +52,36 @@ class MainActivity : AppCompatActivity() {
                 info?.let {
                     if (it.state.isFinished) {
                         showResult("Second process is done")
+                        launchNotificationService()
                     }
                 }
             }
     }
-//Build the data into the correct format before passing it to the worker as input
     private fun getIdInputData(IdKey: String, IdValue: String) =
         Data.Builder()
             .putString(IdKey, IdValue)
             .build()
-    //Show the result as toast
     private fun showResult(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+    private fun launchNotificationService() {
+        NotificationService.trackingCompletion.observe(
+            this) { Id ->
+            showResult("Process for Notification Channel ID $Id is done!")
+        }
+        val serviceIntent = Intent(
+            this,
+            NotificationService::class.java
+        ).apply {
+            putExtra(EXTRA_ID, "001")
+        }
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+    companion object{
+        const val EXTRA_ID = "Id"
+    }
+
 }
+
+
+
